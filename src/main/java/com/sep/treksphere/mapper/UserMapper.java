123b18551b028bf5223ad4400815b0945a0b2bbe
Duong Manh Huy <manhhuy2104@gmail.com>
2026-07-04 15:42:49 +0700

    chore: use roleMapper instead of map role of UserMapper

--- src/main/java/com/sep/treksphere/mapper/UserMapper.java
+++ src/main/java/com/sep/treksphere/mapper/UserMapper.java
@@ -2,15 +2,9 @@ package com.sep.treksphere.mapper;
 
 import com.sep.treksphere.dto.response.UserProfileResponse;
 import com.sep.treksphere.dto.response.UserResponse;
-import com.sep.treksphere.entity.Role;
 import com.sep.treksphere.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = {RoleMapper.class})
 public interface UserMapper {
@@ -19,16 +13,6 @@ public interface UserMapper {
     @Mapping(target = "roles", source = "roles")
     UserResponse toUserResponse(User user);
 
-    @Mapping(target = "roles", source = "roles", qualifiedByName = "mapRoles")
+    @Mapping(target = "roles", source = "roles")
     UserProfileResponse toUserProfileResponse(User user);
-
-    @Named("mapRoles")
-    default List<String> mapRoles(Set<Role> roles) {
-        if (roles == null) {
-            return null;
-        }
-        return roles.stream()
-                .map(Role::getRoleName)
-                .collect(Collectors.toList());
-    }
 }
