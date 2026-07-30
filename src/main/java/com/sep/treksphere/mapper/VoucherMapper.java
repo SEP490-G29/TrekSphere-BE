cf05c2828e4a4496be8cb31aab664438573c068c
dotuananh <dotuananh1625@gmail.com>
2026-07-30 19:15:33 +0700

    feat: implement vendor voucher create, update, and delete APIs

--- src/main/java/com/sep/treksphere/mapper/VoucherMapper.java
+++ src/main/java/com/sep/treksphere/mapper/VoucherMapper.java
@@ -4,8 +4,18 @@ import com.sep.treksphere.dto.response.VoucherResponse;
 import com.sep.treksphere.entity.Voucher;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import com.sep.treksphere.dto.request.CreateVoucherRequest;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import com.sep.treksphere.dto.request.UpdateVoucherRequest;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface VoucherMapper {
     VoucherResponse toVoucherResponse(Voucher voucher);
+    Voucher toVoucher(CreateVoucherRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateVoucherFromRequest(UpdateVoucherRequest request, @MappingTarget Voucher voucher);
 }
