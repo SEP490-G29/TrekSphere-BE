ef7419e65be2cbce2441949b63476376c406fd23
dotuananh <dotuananh1625@gmail.com>
2026-09-12 09:19:25 +0700

    fix(message-constant) - resolve conflict

--- src/main/java/com/sep/treksphere/common/constant/MessageConstant.java
+++ src/main/java/com/sep/treksphere/common/constant/MessageConstant.java
@@ -580,9 +580,7 @@ public class MessageConstant {
     public static final String REPORT_TARGET_TYPE_REQUIRED = "Loại nội dung báo cáo không được để trống";
     public static final String REPORT_REASON_REQUIRED = "Lý do báo cáo không được để trống";
     public static final String REPORT_ACTION_REQUIRED = "Hành động xử lý không được để trống";
-    public static final String REPORT_NOT_FOUND = "Báo cáo không tồn tại";
-    public static final String REPORT_TARGET_NOT_FOUND = "Đối tượng bị báo cáo không tồn tại";
-    public static final String REPORT_ALREADY_RESOLVED = "Báo cáo này đã được xử lý từ trước";
+
 
     // Notification Messages
     public static final String NOTIFICATIONS_FETCHED_SUCCESS = "Lấy danh sách thông báo thành công";
